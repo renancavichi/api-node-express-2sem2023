4765d8c3baedc23b0d1e6bc679594b6465a7e6d6
Renan Cavichi <renancavichi@gmail.com>
2023-08-07 18:05:20 -0300

    create server api with node and express

--- src/server.js
+++ src/server.js
@@ -1,1 +1,11 @@
-console.log('Rodando servidor...')
+const express = require('express')
+
+const api = express()
+
+api.get('/', (req, res) => {
+    res.json({message: "Bem-vindo a nossa API"})
+})
+
+api.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000. http://localhost:3000')
+})
