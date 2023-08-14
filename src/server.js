e8ce1cc1656e9105efff4b5fd1f5dbd11aa537ad
Renan Cavichi <renancavichi@gmail.com>
2023-08-14 15:29:03 -0300

    add router user

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 //const express = require('express')
 import express from 'express'
+import userRouter from './routes/userRouter.js'
 
 const api = express()
 
 api.get('/', (req, res) => {
     res.json({message: "Bem-vindo a nossa API"})
 })
+
+api.use('/user', userRouter)
 
 api.listen(3000, () => {
     console.log('Servidor rodando na porta 3000! http://localhost:3000')
