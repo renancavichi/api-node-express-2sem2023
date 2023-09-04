a45ca0f5d2ea6d86af70f735392a384d63115e5c
Renan Cavichi <renancavichi@gmail.com>
2023-09-04 15:50:56 -0300

    create user route

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -4,4 +4,9 @@ const getById = async (id) => {
     return await db.query("SELECT name, email FROM users Where id = ?", [id])
 }
 
-export default {getById}
+const create = async (user) => {
+    const {name, email, pass} = user
+    return await db.query("INSERT INTO users (name, email, pass) VALUES (?, ?, ?);", [name, email, pass])
+}
+
+export default {getById, create}
